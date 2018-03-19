4eee750bc81e6c0781cb2151f56036d288162e19
Isaac Hutchison <hutchinsongalax@gmail.com>
2018-03-19 14:52:27 -0400

    Rough student progress page completed. Could use some good CSS to make it look pretty.

--- src/components/utilities/regProgressBar.js
+++ src/components/utilities/regProgressBar.js
@@ -1,32 +1,80 @@
 import React from 'react';
+import "./regProgressBarStyle.css";
 
 
 class regProgressBar extends React.Component {
 
-    // This is still under development. -Isaac 
+    stepOneComp = "Incomplete";
+    stepTwoComp = "Incomplete";
+    stepThreeComp = "Incomplete";
+    stepFourComp = "Incomplete";
+    stepFiveComp = "Incomplete";
+    stepFinalComp = "Incomplete";
+
+
+    regCheckFunction(step1, step2, step3, step4, step5, stepFinal) {
+        if (step1) {
+            this.stepOneComp = "Complete";
+            if (step2) {
+                this.stepTwoComp = "Complete";
+                if (step3) {
+                    this.stepThreeComp = "Complete";
+                    if (step4) {
+                        this.stepFourComp = "Complete";
+                        if (step5) {
+                            this.stepFiveComp = "Complete";
+                            if (stepFinal) {
+                                this.stepFinalComp = "Complete";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    // This is still under development. -Isaac
     render() {
         return (
             <div className="container">
                 <h1>Student Registration Progress</h1>
-                <table>
+
+                <br/>
+                <br/>
+                {this.regCheckFunction(true,true,false,false,false,false)};
+                <table className="table" id="table">
                     <tr>
-                        <th> Step 1: CAS Sign Up Approval</th>
-                        <th> Step 2: General Registration</th>
-                        <th> Step 3: Request for Accommodations</th>
-                        <th> Step 4: Policies and Procedures for Receiving Services</th>
-                        <th> Step 5: Academic Accommodations Form</th>
-                        <th> Registration Complete</th>
+                        <th>Registration Step</th>
+                        <th>Completion Status</th>
                     </tr>
                     <tr>
-                        <td>Incomplete</td>
-                        <td>Incomplete</td>
-                        <td>Incomplete</td>
-                        <td>Incomplete</td>
-                        <td>Incomplete</td>
-                        <td>Incomplete</td>
+                        <th> Step 1: CAS Sign Up Approval</th>
+                        <td id="stepOne">{this.stepOneComp}</td>
+                    </tr>
+                    <tr>
+                        <th> Step 2: General Registration</th>
+                        <td id="stepTwo">{this.stepTwoComp}</td>
+                    </tr>
+                    <tr>
+                        <th> Step 3: Request for Accommodations</th>
+                        <td id="stepThree">{this.stepThreeComp}</td>
+                    </tr>
+                    <tr>
+                        <th> Step 4: Policies and Procedures for Receiving Services</th>
+                        <td id="stepFour">{this.stepFourComp}</td>
+                    </tr>
+                    <tr>
+                        <th> Step 5: Academic Accommodations Form</th>
+                        <td id="stepFive">{this.stepFiveComp}</td>
+                    </tr>
+                    <tr>
+                        <th> Registration Complete</th>
+                        <td id="regComp">{this.stepFinalComp}</td>
                     </tr>
                 </table>
             </div>
+
         );
     };
 
