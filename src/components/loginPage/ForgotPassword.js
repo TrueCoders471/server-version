41d1da80e69bbf82569461712e146226eb6eb5b2
Isaac Hutchison <hutchinsongalax@gmail.com>
2018-03-22 09:33:04 -0400

    Fixed the forgot password page. Redesigned it.

--- src/components/loginPage/ForgotPassword.js
+++ src/components/loginPage/ForgotPassword.js
@@ -4,36 +4,68 @@ import './ForgotPassword-Styles.css';
 
 class ForgotPassword extends React.Component {
 
+    drawLogBackground() {
+        return (<div id="loginBackgroundStretch" src>
+            <img className="background-image" src={require('./../../public/images/loginBackground.jpg')}/>
+        </div>);
+    }
+
     resetPassword() {
         alert("An E-mail has been sent to the E-mail that was entered.");
     }
 
     render() {
         return (
-            <div id="container" className="forgot-password">
+            <div id="container">
+                {this.drawLogBackground()}
                 <form className="form-signin">
                     <br/><br/>
-                    <div id="Fields-container">
-                        <div id="top-div-Container">
-                            <h2 className="form-signin-header">Forgot Password</h2>
+                    <div className="Fields-container">
+                        <div id="primary">
+                            <form>
+                                <fieldset>
+                                    <div>
+                                        <div className="form-group col-md-12">
+                                            <legend><h1 className="text-center"><b>Forgot Password</b></h1></legend>
+                                        </div>
+                                        <div className="form-group col-md-12">
+                                            <h5 className="text-center">Enter the E-mail address that was used to create the account, an E-mail will then be sent to the
+                                                specified E-mail address that will allow you to reset your password.</h5>
+                                        </div>
+                                        <div className="form-group col-md-12" align="middle">
+                                            <input type="text" id="inputUserName" className="form-control" required
+                                                   placeholder="RU-Email" autoFocus/>
+                                        </div>
+                                        <div className="form-group col-md-12" align="middle">
+                                            <button className="btn btn-lg btn-primary" type="submit" onClick={this.resetPassword}>Submit</button>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </form>
                         </div>
-
-
-                        <br/><br/>
-
-                        <h5>Enter the E-mail address that was used to create the account, an E-mail will then be sent to the
-                            specified E-mail address that will allow you to reset your password.</h5>
-
-                        <input type="text" id="UserEmail" className="form-control" placeholder="RU Email" required
-                                autoFocus/>
-                        <br/><br/>
-
-                        <button className="btn btn-lg btn-primary" type="submit" onClick={this.resetPassword}>Submit</button>
                     </div>
+
                 </form>
             </div>
         );
     }
 }
+
+{/*<div id="top-div-Container">
+    <h2 className="form-signin-header">Forgot Password</h2>
+</div>
+
+
+<br/><br/>
+
+<h5>Enter the E-mail address that was used to create the account, an E-mail will then be sent to the
+specified E-mail address that will allow you to reset your password.</h5>
+
+<input type="text" id="UserEmail" className="form-control" placeholder="RU Email" required
+       autoFocus/>
+<br/><br/>
+
+<button className="btn btn-lg btn-primary" type="submit" onClick={this.resetPassword}>Submit</button>
+</div>*/}
 
 export default ForgotPassword;
