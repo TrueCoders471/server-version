8bc02fbff406a37a6ecb7c6f83f25119f6134bb4
Brandon Phillips <bphillips2@radford.edu>
2018-04-10 14:35:02 -0400

    - Deleted the myTestCardComp.js file. - Deleted the myTestCardCompstyles.css file. (Sorry Isaac) - Now the class cards that are being rendered on the 'testForIsaac' page are being created using a loop and arrays, ready for data! - I also added a ton of specific CSS that styles these pages.
    ...More to come as far as styles go!

--- src/components/adminPage/testPage.js
+++ src/components/adminPage/testPage.js
@@ -1,61 +1,61 @@
 import React from "react";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import CardExampleWithAvatar from './myTestCardComp';
 import "./testPageStyles.css";
 
 //!!!!!!!! Still in Development !!!!!!!!!!
 class testPage extends React.Component{
 
+    courses = ["ITEC 220", "GEO 100", "ITEC 380"];
+    descs = ["Computer Science II", "Intro to Geology", "Racket"];
+
+    classCard(courseName, courseDesc){
+        return (
+            <div className={"class-column"}>
+                <div className={"image-class-header"}>
+                    <img className={"image-header"} src = {require ('./../../public/images/testClassImage.jpeg')} alt="" />
+                    <br/><br/>
+                </div>
+                <div className={"classInfo-container"}>
+                    <span className={"courseName-style"}>{courseName}</span> <br/>
+                    <span className={"courseDesc-style"}>{courseDesc}</span>
+                </div>
+                <div className={"button-container"}>
+                    <button className={"commonButton"}>ENTER</button>
+                </div>
+            </div>
+        );
+    }
+
+    renderMultiple(arrayCourse, arrayDesc){
+        var cards = [];
+        for (var i = 0; i < arrayCourse.length; i++) {
+            cards.push(this.classCard(arrayCourse[i], arrayDesc[i]));
+        }
+        return cards;
+    }
+
+    //THIS AREA IS A WORK IN PROGRESS
+    /*getCourseName(){
+        This is the function that will get the course name from the db MAYBE
+    }
+
+    getCourseDesc(){
+        This is the function that will get the course description from the db MAYBE
+    }
+
+    getCourseIMG(){
+        This is the function that will get the course IMG from the db MAYBE
+    }*/
+
 
     render() {
 
         return (
-            <div className="container">
-                <div className="class-row">
-                    <div className="class-column">
-            <MuiThemeProvider>
-                <CardExampleWithAvatar/>
-            </MuiThemeProvider>
-                    </div>
-                    <div className="class-column">
-                        <MuiThemeProvider>
-                            <CardExampleWithAvatar/>
-                        </MuiThemeProvider>
-                    </div>
-                    <div className="class-column">
-                        <MuiThemeProvider>
-                            <CardExampleWithAvatar/>
-                        </MuiThemeProvider>
-                    </div>
-                    <div className="class-column">
-                        <MuiThemeProvider>
-                            <CardExampleWithAvatar/>
-                        </MuiThemeProvider>
-                    </div>
-                    <div className="class-column">
-                        <MuiThemeProvider>
-                            <CardExampleWithAvatar/>
-                        </MuiThemeProvider>
-                    </div>
-                    <div className="class-column">
-                        <MuiThemeProvider>
-                            <CardExampleWithAvatar/>
-                        </MuiThemeProvider>
-                    </div>
-                    <div className="class-column">
-                        <MuiThemeProvider>
-                            <CardExampleWithAvatar/>
-                        </MuiThemeProvider>
-                    </div>
-                    <div className="class-column">
-                        <MuiThemeProvider>
-                            <CardExampleWithAvatar/>
-                        </MuiThemeProvider>
-                    </div>
+            <div className={"container"}>
+                <div className={"classCard-container"}>
+                    {this.renderMultiple(this.courses, this.descs)}
                 </div>
-                <div className="class-row"></div>
-                <br />
             </div>
+
         );
     }
 
